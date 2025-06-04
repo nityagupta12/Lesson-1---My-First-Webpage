f005ed046f43fd482863155d6f39a07d44713b0b
nityagupta12 <guptanitya20@gmail.com>
2025-06-04 16:40:23 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function add(){
+
+    a = 35;
+    b = 56;
+    document.getElementById("demo").innerHTML = a + b
+}
+
+function subtract(){
+    a = 35;
+    b = 56;
+    document.getElementById("demo").innerHTML = a - b
+}
+
+function multiply(){
+    a = 35;
+    b = 56;
+    document.getElementById("demo").innerHTML = a * b
+}
+
+function divide(){
+    a = 35;
+    b = 56;
+    document.getElementById("demo").innerHTML = a / b
+}
+
