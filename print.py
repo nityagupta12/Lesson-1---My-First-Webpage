6b5a68b14b053174167d6af35d6551f4f1954d5e
nityagupta12 <guptanitya20@gmail.com>
2025-07-24 09:58:57 +0400

    Add files via upload

---
+++ print.py
@@ -0,0 +1,14 @@
+#Print a Sentence
+print("Welcome to the world of programming")
+
+#Print a Number
+print(4)
+
+#Next Line after Print
+print("Hello Python \n")
+
+#Pass multiplpe arguments for printing
+print("hello", 8)
+
+#End argument of Print Statement
+print("welcome to", end= "*")
---
+++ var.py
@@ -0,0 +1,17 @@
+#Creating different variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+#User Input
+name = input("enter your name: ")
+
+print("\nHello", name, "\nwelcome to codingal")
+
+age = int(input("please enter your age: "))
+
+print(age)
