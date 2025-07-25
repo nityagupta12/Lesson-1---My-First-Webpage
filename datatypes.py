ef714314dd08e6824d460857f8ab60af1e6a33e8
nityagupta12 <guptanitya20@gmail.com>
2025-07-25 09:44:42 +0400

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,9 @@
+a = 2012
+b = "Nitya"
+c = False
+d = 0.8
+
+print("Data types of first value is", type (a))
+print("Data types of second value is", type (b))
+print("Data types of third value is", type (c))
+print("Data types of fourth value is", type (d))
