7788b13909efad67597d816d219e4cb39e0674fb
nityagupta12 <guptanitya20@gmail.com>
2025-07-29 10:00:21 +0400

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,13 @@
+weather = input("Please enter the weather:  ")
+
+if weather =="summer":
+    print("It is too hot")
+    
+elif weather =="winter":
+    print("It is too cold")
+    
+elif weather =="autumn":
+    print("It is windy")
+    
+elif weather =="spring":
+    print(" It is raining")
