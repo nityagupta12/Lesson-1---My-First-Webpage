200fc7bfb93db4452af16fb5357ed0a789efbca7
nityagupta12 <guptanitya20@gmail.com>
2025-07-30 09:56:01 +0400

    Add files via upload

---
+++ and.py
@@ -0,0 +1,9 @@
+a = 12
+b = 13
+c = 23
+
+if a and b == 12 :
+    print("A and B are equal")
+    
+else:
+    print("They are not equal")
---
+++ or.py
@@ -0,0 +1,9 @@
+a = 19
+b = 13
+c = 23
+
+if a or b == 19 :
+    print("A or B are equal")
+    
+else:
+    print("They are not equal")
