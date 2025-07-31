c33196f166dc409540b65072e2f9f7d86f5c94ed
nityagupta12 <guptanitya20@gmail.com>
2025-07-31 09:57:35 +0400

    Add files via upload

---
+++ bitwise.py
@@ -0,0 +1,17 @@
+a = 10
+b = -10
+
+# print bitwise right shift operator
+print("a >> 1 =", a >> 1)
+print("b >> 1 =", b >> 1)
+
+# in right shift number gets divided by 2
+
+a = 5
+b = -10
+
+#print bitwise left shift operator 
+print("a << 1 =", a << 1)
+print("b << 1 =", b << 1)
+
+# in left shift number gets multiplied by 2
