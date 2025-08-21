ec3be17bbb21767f6a93ada1907696cc1de326cb
nityagupta12 <guptanitya20@gmail.com>
2025-08-21 09:57:49 +0400

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,5 @@
+def well_wishes():
+    print("Hello")
+    print("How are you")
+    print("What are you doing")
+well_wishes()
---
+++ weather conditions.py
@@ -0,0 +1,7 @@
+def weather_conditions():
+  print("The weather is:  ")
+  print("Spring", spring)
+  print("Autumn", autumn)
+spring = "in Dubai is very humid and dry! 🥵"
+autumn = "in Dubai is quite windy and cool! 🪭"
+weather_conditions()
