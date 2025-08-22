94b6dc08b5b9fb1865ef851a51bdf2f31c931743
nityagupta12 <guptanitya20@gmail.com>
2025-08-22 09:55:17 +0400

    Add files via upload

---
+++ name.py
@@ -0,0 +1,11 @@
+def greet(name,age,hobby):
+    print("Hello, Welcome to Coding", name)
+    print("Your age is: ", age)
+    print("Your hobby is: ", hobby)
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+hobby = input("Enter your hobby: ")
+greet(name,age,hobby)
+
+
+
