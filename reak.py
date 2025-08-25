3901956b1c9bec1a78b0e0447cc7d36d3d9a1500
nityagupta12 <guptanitya20@gmail.com>
2025-08-25 12:44:36 +0400

    Add files via upload

---
+++ reak.py
@@ -0,0 +1,11 @@
+#take user input
+a = input ("Enter a word: ")
+#program to check break keyword
+for i in a: #iterate for loop
+    if (i == 'A'): #condtion 1
+        #display result
+        print("A is found")
+        break #break statement 
+    else:
+        #display result
+        print("A not found")
