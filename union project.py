078182a89eea28bc74b87b73b61185e4a7af4d5b
nityagupta12 <guptanitya20@gmail.com>
2025-09-08 16:31:39 +0400

    Add files via upload

---
+++ union project.py
@@ -0,0 +1,9 @@
+seta = { 5,8,9,2,3,5,7,8}
+setb = { 8,9,0,2,3,7}
+
+print("Original set elements: ")
+print(seta)
+print(setb)
+print("\nUnion of two said sets: ")
+setz = seta.union(setb)
+print(setz)
---
+++ union.py
@@ -0,0 +1,9 @@
+setn = {"purple", "orange"}
+seto = {"orange", "turquoise"}
+
+print("Original set elements: ")
+print(setn)
+print(seto)
+print("\nUnion of two said sets: ")
+setz = setn.union(seto)
+print(setz)
